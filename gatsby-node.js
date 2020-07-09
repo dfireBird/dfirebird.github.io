441410b928fe5a9682fbe7876c6c4a083f7ade9a
dfireBird <aadhishrm11@gmail.com>
2020-07-09 11:15:10 +0530

    👌 IMPROVE: changed projects pages source from mdx to json

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const { createFilePath } = require("gatsby-source-filesystem")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-  if (node.internal.type === "Mdx") {
+  if (node.internal.type === "Mdx" || node.internal.type === "ProjectsJson") {
     const path = createFilePath({ node, getNode }).split("/")
     const fileName = path[path.length - 2]
     const postType = path[path.length - 3]
@@ -50,6 +50,15 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      allProjectsJson {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
     }
   `)
 
@@ -59,8 +68,6 @@ exports.createPages = async ({ graphql, actions }) => {
       component = path.resolve("./src/templates/about.jsx")
     } else if (node.fields.slug.includes("post")) {
       component = path.resolve("./src/templates/blog-post.jsx")
-    } else if (node.fields.slug.includes("project")) {
-      component = path.resolve("./src/templates/projects.jsx")
     }
     createPage({
       path: node.fields.slug,
@@ -68,6 +75,13 @@ exports.createPages = async ({ graphql, actions }) => {
       context: {
         slug: node.fields.slug,
       },
+    })
+  })
+
+  result.data.allProjectsJson.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/projects.jsx"),
     })
   })
 
