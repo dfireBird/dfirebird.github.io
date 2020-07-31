343e4cb0858c789a3f17c564eacc9a7bfeec85d9
dfireBird <dfireBird@users.noreply.github.com>
2020-07-31 00:41:09 +0000

    deploy: f41cacdd3671b4ecd967e97ab06b44d4f701fa34

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-828acb1d7062c40ee34a.js"
   },
   {
-    "url": "styles.5baea0bfd5db8d3df72c.css"
+    "url": "styles.b8c97af38227a3d7535f.css"
   },
   {
     "url": "styles-9b6f388623a2ec93d35f.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-9fe058f4359556db0d38.js"
   },
   {
-    "url": "app-88a411bf379244c0e5ab.js"
+    "url": "app-0edf7efb8b7b3c913347.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "716bcb5b03089693ab971e1bb47569b7"
+    "revision": "c143222a859a7b83425f034521b9d99e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-06e676c3cd7b78941a0b.js"
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-88a411bf379244c0e5ab.js`))) {
+  if (!resources || !(await caches.match(`/app-0edf7efb8b7b3c913347.js`))) {
     return await fetch(event.request)
   }
 
