49257469a65dd90a035713f921423adad678ef5a
dfireBird <dfireBird@users.noreply.github.com>
2020-08-30 00:33:03 +0000

    deploy: 7eaef94150dc96eb7932fcc5dd0741b8c6494776

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7d13c320cf9494f09bd8.js"
+    "url": "webpack-runtime-6a2654cc84d0fad40ca0.js"
   },
   {
     "url": "styles.b8c97af38227a3d7535f.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ad8a6af8999133312305dbcb6218bcc9"
+    "revision": "bf124172be90d4741c88330b003f014b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-06e676c3cd7b78941a0b.js"
